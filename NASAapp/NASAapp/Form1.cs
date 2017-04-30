76b9509358d12b1806113bb49bdc456ec62845c2
Ivan <ivanstoickov95@gmail.com>
2017-04-30 08:55:15 +0200

    UI fix

---
+++ NASAapp/NASAapp/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NASAapp
+{
+    public partial class Form1 : Form
+    {
+        List<Image> img;
+        public Objects objects = new Objects();
+        public List<RootObject> RootObjects;
+        public List<RootObject> added;
+        public Functions functions;
+
+        public Form1()
+        {
+            InitializeComponent();
+            RootObjects = new List<RootObject>();
+            added = new List<RootObject>();
+            functions = new Functions();
+            img = new List<Image>();
+            objects = functions.ParseJSON("all");
+            btnAbout.BackColor = System.Drawing.ColorTranslator.FromHtml("#a2ce6c");
+            }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e) {
+
+            panelAbout.Visible = true;
+            panelAddNewLocation.Visible = false;
+            panelAddNewLandslide.Visible = false;
+            panelNearby.Visible = false;
+
+            listBox2.Items.Clear();
+            listBox4.Items.Clear();
+            
+            btnAbout.BackColor=System.Drawing.ColorTranslator.FromHtml("#a2ce6c");
+            buttonAddNewLocation.BackColor = System.Drawing.ColorTranslator.FromHtml("133, 191, 68");
+            buttonNearby.BackColor = System.Drawing.ColorTranslator.FromHtml("133, 191, 68");
+            btnAddNewLandslide.BackColor = System.Drawing.ColorTranslator.FromHtml("133, 191, 68");
+
+        }
+
+        private void buttonCurrentLocation_Click(object sender, EventArgs e)
+        {
+            panelAddNewLocation.Visible = false;
+            panelAbout.Visible = false;
+            panelAddNewLandslide.Visible = false;
+            panelNearby.Visible = false;
+
+            btnAbout.BackColor = System.Drawing.ColorTranslator.FromHtml("133, 191, 68");
+            buttonAddNewLocation.BackColor = System.Drawing.ColorTranslator.FromHtml("#a2ce6c");
+            buttonNearby.BackColor = System.Drawing.ColorTranslator.FromHtml("133, 191, 68");
+            btnAddNewLandslide.BackColor = System.Drawing.ColorTranslator.FromHtml("133, 191, 68");
+        }
+
+        private void btnAddNewLandslide_Click(object sender, EventArgs e)
+        {
+            panelAddNewLandslide.Visible = true;
+            panelAbout.Visible = false;
+            panelAddNewLocation.Visible = false;
+            panelNearby.Visible = false;
+
+            listBox2.Items.Clear();
+
+            btnAbout.BackColor = System.Drawing.ColorTranslator.FromHtml("133, 191, 68");
+            buttonAddNewLocation.BackColor = System.Drawing.ColorTranslator.FromHtml("133, 191, 68");
+            buttonNearby.BackColor = System.Drawing.ColorTranslator.FromHtml("133, 191, 68");
+            btnAddNewLandslide.BackColor = System.Drawing.ColorTranslator.FromHtml("#a2ce6c");
+        }
+
+        private void buttonAddNewLocation_Click(object sender, EventArgs e)
+        {
+            panelAddNewLocation.Visible = true;
+            panelAbout.Visible = false;
+            panelNearby.Visible = false;
+            panelAddNewLandslide.Visible = false;
+            
+            listBox4.Items.Clear();
+
+            btnAbout.BackColor = System.Drawing.ColorTranslator.FromHtml("#85bf44");
+            buttonAddNewLocation.BackColor = System.Drawing.ColorTranslator.FromHtml("#a2ce6c");
+            buttonNearby.BackColor = System.Drawing.ColorTranslator.FromHtml("#85bf44");
+            btnAddNewLandslide.BackColor = System.Drawing.ColorTranslator.FromHtml("#85bf44");
+
+            
+        }
+
+        private void buttonNearby_Click(object sender, EventArgs e)
+        {
+            panelNearby.Visible = true;
+            panelAbout.Visible = false;
+            panelAddNewLocation.Visible = false;
+            panelAddNewLandslide.Visible = false;
+
+            btnAbout.BackColor = System.Drawing.ColorTranslator.FromHtml("#85bf44");
+            buttonAddNewLocation.BackColor = System.Drawing.ColorTranslator.FromHtml("#85bf44");
+            buttonNearby.BackColor = System.Drawing.ColorTranslator.FromHtml("#a2ce6c");
+            btnAddNewLandslide.BackColor = System.Drawing.ColorTranslator.FromHtml("#85bf44");
+            
+            listBox4.Items.Clear();
+
+            //List<RootObject> p = functions.nearby(RootObjects, 250, 
+            //    functions.GetLocation(functions.MyIP()).Split(' ')[0], 
+            //    functions.GetLocation(functions.MyIP()).Split(' ')[1]);
+            Objects p = functions.ParseJSON("Macedonia");
+
+            foreach (RootObject ro in p.obj) {
+                listBox4.Items.Add(ro.ToString());
+            }
+        }
+
+        private void btnShowLandslidesDetails_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (textBox5.Text.Equals("") && textBox6.Text.Equals("") && textBox7.Text.Equals(""))
+            {
+                System.Windows.Forms.MessageBox.Show("Please enter location or latitude and longitude!", "Unsufficient data", MessageBoxButtons.OK);
+                //message box
+            }
+            string latitude;
+            string longitude;
+            string location = textBox1.Text;
+            if (!checkBox1.Checked)
+            {
+                latitude = textBox6.Text;
+                longitude = textBox7.Text;
+                List<RootObject> RootObjects = functions.nearby(this.RootObjects, 250, latitude, longitude);
+            }
+            else
+            {
+                string tmp = functions.GetLocation(functions.MyIP());
+                latitude = tmp.Split(' ')[0];
+                longitude = tmp.Split(' ')[1];
+                List<RootObject> RootObjects = functions.nearby(this.RootObjects, 250, functions.GetLocation(functions.MyIP()).Split(' ')[0], functions.GetLocation(functions.MyIP()).Split(' ')[1]);
+            }
+            added.Add(new RootObject(location, latitude, longitude));
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                textBox6.Text = "";
+                textBox7.Text = "";
+                textBox6.ReadOnly = true;
+                textBox7.ReadOnly = true;
+            }
+            else
+            {
+                textBox6.Text = "Add latitude..";
+                textBox7.Text = "Add longitude..";
+                textBox6.ReadOnly = false;
+                textBox7.ReadOnly = false;
+            }
+        }
+
+        private void btnDismiss_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            checkBox2.Checked = false;
+
+            panelAbout.Visible = true;
+            panelAddNewLocation.Visible = false;
+            panelAddNewLandslide.Visible = false;
+            panelNearby.Visible = false;
+
+            btnAbout.BackColor = System.Drawing.ColorTranslator.FromHtml("#a2ce6c");
+            buttonAddNewLocation.BackColor = System.Drawing.ColorTranslator.FromHtml("#85bf44");
+            buttonNearby.BackColor = System.Drawing.ColorTranslator.FromHtml("#85bf44");
+            btnAddNewLandslide.BackColor = System.Drawing.ColorTranslator.FromHtml("#85bf44");
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            panelAbout.Visible = true;
+            panelAddNewLocation.Visible = false;
+            panelAddNewLandslide.Visible = false;
+            panelNearby.Visible = false;
+
+            btnAbout.BackColor = System.Drawing.ColorTranslator.FromHtml("#a2ce6c");
+            buttonAddNewLocation.BackColor = System.Drawing.ColorTranslator.FromHtml("#85bf44");
+            buttonNearby.BackColor = System.Drawing.ColorTranslator.FromHtml("#85bf44");
+            btnAddNewLandslide.BackColor = System.Drawing.ColorTranslator.FromHtml("#85bf44");
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            string place = textBox2.Text.Trim();
+            List<RootObject> list = new List<RootObject>();
+            List<RootObject> RootObjects = functions.ParseJSON("all").obj;
+
+            if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                //Message box
+                System.Windows.Forms.MessageBox.Show("Please enter location or latitude and longitude!", "Unsufficient data", MessageBoxButtons.OK);
+                return;
+            }
+            else if (textBox2.Text != "" && textBox2.Text != null)
+            {
+                list = functions.ParseJSON(textBox2.Text).obj;
+            }
+
+            foreach (RootObject ro in list)
+            {
+                listBox2.Items.Add(ro.ToString());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "JPG(*.JPG)|*.jpg|PNG(*.png)|*.jpg|JPEG(*.JPEG)|*.jpeg";
+
+            if (dialog.ShowDialog() == DialogResult.OK) {
+                Image i = Image.FromFile(dialog.FileName);
+                img.Add(i);
+                pictureBox1.Image = i;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                button1.Visible = true;
+            }
+            else
+            {
+                button1.Visible = false;
+            }
+        }
+    }
+}
---
+++ NASAapp/NASAapp/Functions.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+
+using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace NASAapp
+{
+    public class IPLocation
+    {
+        public string @as { get; set; }
+        public string city { get; set; }
+        public string country { get; set; }
+        public string countryCode { get; set; }
+        public string isp { get; set; }
+        public double lat { get; set; }
+        public double lon { get; set; }
+        public string org { get; set; }
+        public string query { get; set; }
+        public string region { get; set; }
+        public string regionName { get; set; }
+        public string status { get; set; }
+        public string timezone { get; set; }
+        public string zip { get; set; }
+
+        public IPLocation() { }
+
+        public IPLocation(string @as, string city, string country, string countryCode, string isp, double lat, double lon, string org, string query, string region, string regionName, string status, string timezone, string zip)
+        {
+            this.@as = @as;
+            this.city = city;
+            this.country = country;
+            this.countryCode = countryCode;
+            this.isp = isp;
+            this.lat = lat;
+            this.lon = lon;
+            this.org = org;
+            this.query = query;
+            this.region = region;
+            this.regionName = regionName;
+            this.status = status;
+            this.timezone = timezone;
+            this.zip = zip;
+        }
+
+        public override string ToString()
+        {
+            return this.lat + " " + this.lon;
+        }
+    }
+
+    public class Functions
+    {
+        public List<RootObject> check(List<RootObject> RootObjects, string RootObject, string Lat, string Lon)
+        {
+            List<RootObject> list = new List<RootObject>();
+            double lat = double.Parse(Lat), lon = double.Parse(Lon);
+
+            foreach (RootObject p in RootObjects)
+                if (p.countryname.Equals(RootObject) || (Math.Abs(Double.Parse(p.latitude) - lat) < 0.5 && (Math.Abs(Double.Parse(p.longitude) - lon) < 0.5)))
+                    list.Add(p);
+
+            return list;
+        }
+
+        public List<RootObject> nearby(List<RootObject> RootObjects, double dist, string Lat, string Lon)
+        {
+            List<RootObject> list = new List<RootObject>();
+            string ip = MyIP();
+
+            if (ip == null)
+            {
+                return null;
+            }
+
+            string[] part = GetLocation(MyIP()).Trim().Split(' ');
+
+            double x1 = double.Parse(part[0]);
+            double y1 = double.Parse(part[1]);
+            double x2 = double.Parse(Lat);
+            double y2 = double.Parse(Lon);
+
+            foreach (RootObject p in RootObjects)
+            {
+                double d = distance(x1, y1, x2, y2);
+                if (d <= 250)
+                    list.Add(p);
+            }
+            return list;
+        }
+
+        public double distance(double lon1, double lat1, double lon2, double lat2)
+        {
+            double R = 6371e3;
+            double fi1 = lat1 * 0.0174533;
+            double fi2 = lat2 * 0.0174533;
+            double delataFi = (lat2 - lat1) * 0.0174533;
+            double delataLambda = (lon2 - lon1) * 0.0174533;
+
+            double a = Math.Sin(delataFi / 2) * Math.Sin(delataFi / 2) +
+                    Math.Cos(fi1) * Math.Cos(fi2) *
+                    Math.Sin(delataLambda / 2) * Math.Sin(delataLambda / 2);
+
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+            double d = R * c;
+
+            return d;
+        }
+
+        public string GetLocation(string ip)
+        {
+            IPLocation data = new IPLocation();
+            try
+            {
+                string info = new WebClient().DownloadString("http://ip-api.com/json/");
+                data = JsonConvert.DeserializeObject<IPLocation>(info);
+                return data.ToString();
+            }
+            catch (Exception)
+            {
+
+            }
+            return "";
+        }
+
+        public string MyIP()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    using (var stream = client.OpenRead("http://www.google.com"))
+                    {
+                        return new WebClient().DownloadString(@"http://icanhazip.com").Trim();
+                    }
+                }
+            }
+            catch
+            {
+                return "No";
+            }
+        }
+
+        public Objects ParseJSON(string query)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+
+            string url = "";
+            if (!query.Equals("all"))
+            {
+                url = "https://data.nasa.gov/resource/tfkf-kniw.json?countryname=" + query;
+            }
+            else
+            {
+                url = "https://data.nasa.gov/resource/tfkf-kniw.json";
+            }
+
+            using (WebClient client = new WebClient())
+            using (Stream stream = client.OpenRead(url))
+            using (StreamReader streamReader = new StreamReader(stream))
+            using (JsonReader reader = new JsonTextReader(streamReader))
+            {
+                reader.SupportMultipleContent = true;
+
+                RootObject RootObjects = new RootObject();
+                var serializer = new JsonSerializer();
+
+                Objects obj = new Objects();
+
+                while (reader.Read())
+                {
+                    if (reader.TokenType == JsonToken.StartObject)
+                    {
+                        RootObject p = serializer.Deserialize<RootObject>(reader);
+                        obj.obj.Add(p);
+                    }
+                }
+
+                return obj;
+            }
+        }
+
+        public List<RootObject> getByYear(List<RootObject> list, double year)
+        {
+            List<RootObject> RootObjects = new List<RootObject>();
+            foreach (RootObject p in list)
+            {
+                if (p.tstamp.Contains(year.ToString()))
+                {
+                    RootObjects.Add(p);
+                }
+            }
+            return RootObjects;
+        }
+
+        public double probability(List<RootObject> list, string lat, string lon)
+        {
+            List<RootObject> nearRootObjects = nearby(list, 200, lat, lon);
+            double P = 0;
+
+
+            return P;
+        }
+    }
+}
